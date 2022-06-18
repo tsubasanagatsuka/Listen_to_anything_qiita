24337b3207cac47f5a011693ec6424c1fccc8fa2
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-06-18 19:17:09 +0900

    検索の指定をtitleからbodyに変えた

--- index.rb
+++ index.rb
@@ -3,8 +3,11 @@ require 'net/https'
 require 'uri'
 
 query = URI.encode_www_form_component(ENV['KEYWORD'])
-uri = URI.parse("https://qiita.com/api/v2/items?query=title:#{query}")
+uri = URI.parse("https://qiita.com/api/v2/items?query=body:#{query}")
 res = Net::HTTP.get_response(uri)
 
-puts JSON.parse(res.response.body)[0]["title"]
-
+# puts JSON.parse(res.response.body)[0.1.2.3.4]["url"]
+# 上のクラスを順番に出力するために下のようにeachさせる
+(0..4).each do |i|
+  puts JSON.parse(res.response.body)[i]["url"]
+end
