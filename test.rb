5ac200b2efb347d65c20af5544e109af541c986d
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-06-20 15:40:33 +0900

    lineプログラム

--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'uri'
-# puts ARGV[0] + "+" + ARGV[1] + "+" + ARGV[2]
-# puts ARGV.size
-# puts "うんこ".size
-puts ARGV.join('+')
-# p ARGV
-# p putsは違う　pは形を変えずに出力　putsは要素ごとに改行して出力　米　ARGV = []
-query = URI.encode_www_form_component(ARGV.join('+'))
-puts query
